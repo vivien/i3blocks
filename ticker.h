0700f2cb72b076fd49217b3c2002cc337946ea57
Bogdan Migunov <bogdanmigunov@yandex.ru>
2023-05-30 05:05:28 +0300

    block; ticker: Update copyright

--- ticker.h
+++ ticker.h
@@ -1,6 +1,6 @@
 /*
  * ticker.h - definition of a ticker
- * Copyright (C) 2022 Bogdan Migunov
+ * Copyright (C) 2022-2023 Bogdan Migunov
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
